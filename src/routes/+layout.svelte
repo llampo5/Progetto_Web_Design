639ad172ef9a60aa7008fcbcece0b5dfb947c9c3
llampo5 <mattiacesare.lampugnani@mail.polimi.it>
2026-03-25 17:12:38 +0100

    Prime modifiche

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -9,3 +9,12 @@
 </svelte:head>
 
 {@render children()}
+<!-- Tutto ciò che mettiamo qui sotto non viene modificato dal CSS nell'altro file -->
+<p>© polimi 2026</p>
+<style>
+/* Se utilizzo il tag :global() viene applicato ovunque, se invece lascio solo
+p{...} si applica solamente a ciò che sta sotto a render children */
+	:global(p){
+    color:brown;
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
+<script>
+  // Qui si può scrivere il JavaScript
+  console.log("Messaggio Numero 54, sto impazzendo, sono rinchiuso da troppo tempo, la mia vita è appesa ad un filo, ho cominciato a drogarmi, ho leccato le pareti e non sono buone, ma in qualche modo dovrò pur mangiare")
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<style>
+/* Qui scrivo il CSS */
+  h1:hover{
+    color:green;
+   
+  }
+</style>
