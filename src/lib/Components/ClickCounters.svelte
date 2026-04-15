909d5175ca22dacdb96729907ebf3e22c0812de7
llampo5 <mattiacesare.lampugnani@mail.polimi.it>
2026-04-15 10:14:59 +0200

    cambiamento auto > static

---
+++ src/lib/Components/ClickCounters.svelte
@@ -0,0 +1,13 @@
+<script>
+  let props = $props();
+  let count = $state(props.initialCount);
+  let doubled = $derived(count * 3);
+</script>
+
+<button onclick={() => count++}>
+  clicks: {count}
+</button>
+
+<span>
+  <p>doubled is {doubled}</p>
+</span>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,23 @@
 <script>
-  // Qui si può scrivere il JavaScript
-  console.log("Messaggio Numero 54, sto impazzendo, sono rinchiuso da troppo tempo, la mia vita è appesa ad un filo, ho cominciato a drogarmi, ho leccato le pareti e non sono buone, ma in qualche modo dovrò pur mangiare")
+// Qui si può scrivere il JavaScript
+  
+  import ClickCounters from "$lib/Components/ClickCounters.svelte"
+  import ProgressiveSum from "$lib/Components/ProgressiveSum.svelte"
+
+  let count = $state(5)
 </script>
 
 <h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<p>
+  Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
+  documentation
+</p>
+
+<ClickCounters initialCount{5}/>
+<p>isello {count}</p>
 <style>
-/* Qui scrivo il CSS */
-  h1:hover{
-    color:green;
-   
+  /* Qui scrivo il CSS */
+  h1:hover {
+    color: green;
   }
 </style>
